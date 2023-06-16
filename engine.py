dc6ccf8d65e1bcaff1cfb01250ed81a5f1142b42
Chance <woncoh1@gmail.com>
2023-06-16 20:16:59 +0900

    Add find_lr()

--- engine.py
+++ engine.py
@@ -2,21 +2,14 @@
 import torch
 
 
-def count_correct_predictions(
-    predictions: torch.Tensor,
-    labels: torch.Tensor,
-) -> int:
-    return predictions.argmax(dim=1).eq(labels).sum().item()
-
-
 def train_step(
-    device: torch.device,
-    loader: torch.utils.data.DataLoader,
-    model: torch.nn.Module,
-    criterion: torch.nn.functional,
-    optimizer: torch.optim.Optimizer,
-    scheduler: torch.optim.lr_scheduler.LRScheduler,
-    epoch: int,
+    device:torch.device,
+    loader:torch.utils.data.DataLoader,
+    model:torch.nn.Module,
+    criterion:torch.nn.functional,
+    optimizer:torch.optim.Optimizer,
+    scheduler:torch.optim.lr_scheduler.LRScheduler,
+    epoch:int,
 ) -> tuple[float, float]:
     model.train()
     train_loss = 0
@@ -53,10 +46,10 @@ def train_step(
 
 
 def test_step(
-    device: torch.device,
-    loader: torch.utils.data.DataLoader,
-    model: torch.nn.Module,
-    criterion: torch.nn.functional,
+    device:torch.device,
+    loader:torch.utils.data.DataLoader,
+    model:torch.nn.Module,
+    criterion:torch.nn.functional,
 ) -> tuple[float, float]:
     model.eval()
     test_loss = 0
@@ -80,14 +73,14 @@ def test_step(
 
 
 def train(
-    device: torch.device,
-    train_loader: torch.utils.data.DataLoader,
-    test_loader: torch.utils.data.DataLoader,
-    model: torch.nn.Module,
-    criterion: torch.nn.functional,
-    optimizer: torch.optim.Optimizer,
-    scheduler: torch.optim.lr_scheduler.LRScheduler,
-    epochs: int,
+    device:torch.device,
+    train_loader:torch.utils.data.DataLoader,
+    test_loader:torch.utils.data.DataLoader,
+    model:torch.nn.Module,
+    criterion:torch.nn.functional,
+    optimizer:torch.optim.Optimizer,
+    scheduler:torch.optim.lr_scheduler.LRScheduler,
+    epochs:int,
 ) -> dict[str, list[float]]:
     results = {
         'train_loss': [],
@@ -118,3 +111,61 @@ def train(
         results['test_acc'].append(test_acc*100)
     print('Learning rate =', params_optimizer['lr'])
     return results
+
+
+def count_correct_predictions(
+    predictions:torch.Tensor,
+    labels:torch.Tensor,
+) -> int:
+    return predictions.argmax(dim=1).eq(labels).sum().item()
+
+
+def find_lr(
+    device:torch.device,
+    train_loader:torch.utils.data.DataLoader,
+    model:torch.nn.Module,
+    optimizer:torch.optim.Optimizer,
+    init_value:float=1e-8,
+    final_value:float=10.,
+    beta: float=0.98,
+) -> tuple[list[float], list[float]]:
+    """https://sgugger.github.io/how-do-you-find-a-good-learning-rate.html"""
+    trn_loader = train_loader
+    net = model
+    num = len(trn_loader) - 1
+    mult = (final_value / init_value) ** (1/num)
+    lr = init_value
+    optimizer.param_groups[0]['lr'] = lr
+    avg_loss = 0.
+    best_loss = 0.
+    batch_num = 0
+    losses = []
+    lrs = []
+    for data in trn_loader:
+        batch_num += 1
+        # Get the loss for this mini-batch of inputs/outputs
+        inputs, labels = data
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+        optimizer.zero_grad()
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        # Compute the smoothed loss
+        avg_loss = beta*avg_loss+(1-beta)*loss.item()
+        smoothed_loss = avg_loss/(1-beta**batch_num)
+        # Stop if the loss is exploding
+        if batch_num > 1 and smoothed_loss > 4*best_loss:
+            return lrs, losses
+        # Record the best loss
+        if smoothed_loss < best_loss or batch_num==1:
+            best_loss = smoothed_loss
+        # Store the values
+        losses.append(smoothed_loss)
+        lrs.append(lr)
+        # Do the SGD step
+        loss.backward()
+        optimizer.step()
+        # Update the lr for the next step
+        lr *= mult
+        optimizer.param_groups[0]['lr'] = lr
+    return lrs, losses
